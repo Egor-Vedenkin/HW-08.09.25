7e9d7d6db94b8c3349b11c9df352a0603e8f86a9
Egor-Vedenkin <egorushkavedenkin@yandex.ru>
2025-09-03 21:05:01 +0300

    Completed second task and upgraded 1'st

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,26 @@ public class Main {
         System.out.println("Значение переменной secondVar с типом byte равно " + secondVar);
         short thirdVar = 30_000;
         System.out.println("Значение переменной thirdVar с типом short равно " + thirdVar);
-        long fourthVar = 20_000_000;
+        long fourthVar = 20_000_000_000_000L;
         System.out.println("Значение переменной fourthVar с типом long равно " + fourthVar);
         float fifthVar = 9.97f;
         System.out.println("Значение переменной fifthVar с типом float равно " + fifthVar);
         double sixthVar = 9.4;
         System.out.println("Значение переменной sixthVar с типом double равно " + sixthVar);
+        // Отступ для визуального разделения задач
+        System.out.println();
 
         // Задача 2
+        float var1 = 27.12f;
+        long var2 = 987_678_965_549L;
+        float var3 = 2.786f;
+        short var4 = 569;
+        short var5 = -159;
+        short var6 = 27897;
+        byte var7 = 67;
+
+        // Задача 3
+
 
     }
 }
