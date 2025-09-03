4d306eb301c4264efcf6c575f0d31fbe6c0db4df
Egor-Vedenkin <egorushkavedenkin@yandex.ru>
2025-09-03 20:12:18 +0300

    First task done

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Задача 1
+        int firstVar = 2_000_000;
+        System.out.println("Значение переменной firstVar с типом int равно " + firstVar);
+        byte secondVar = -7;
+        System.out.println("Значение переменной secondVar с типом byte равно " + secondVar);
+        short thirdVar = 30_000;
+        System.out.println("Значение переменной thirdVar с типом short равно " + thirdVar);
+        long fourthVar = 20_000_000;
+        System.out.println("Значение переменной fourthVar с типом long равно " + fourthVar);
+        float fifthVar = 9.97f;
+        System.out.println("Значение переменной fifthVar с типом float равно " + fifthVar);
+        double sixthVar = 9.4;
+        System.out.println("Значение переменной sixthVar с типом double равно " + sixthVar);
+
+        // Задача 2
+
     }
 }
